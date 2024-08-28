9498356439bd988a2775d19cbb880dbdcdb6cb00
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 12:09:01 +0200

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,6 @@
+DATA_PATH = 'data/'
+RAW_DATA_PATH = os.path.join(DATA_PATH, 'raw/')
+PROCESSED_DATA_PATH = os.path.join(DATA_PATH, 'processed/')
+TRAIN_TEST_SPLIT_PATH = os.path.join(DATA_PATH, 'train_test_split/')
+LOGS_PATH = 'logs/'
+RESULTS_PATH = 'results/'
