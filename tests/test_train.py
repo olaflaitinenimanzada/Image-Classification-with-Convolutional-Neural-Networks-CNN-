d45de2d1475a7e7337039474c69e637841b3e8b5
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 12:11:05 +0200

    Create test_train.py

---
+++ tests/test_train.py
@@ -0,0 +1,10 @@
+import unittest
+from src.train import train_model
+
+class TestTrain(unittest.TestCase):
+    def test_train_model(self):
+        # Test model eğitme fonksiyonu
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
