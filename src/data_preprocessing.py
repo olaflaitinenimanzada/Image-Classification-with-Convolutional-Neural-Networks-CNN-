aba2fd41c46c647ade795e23916ac1188c78573a
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 12:02:49 +0200

    Create data_preprocessing.py

---
+++ src/data_preprocessing.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def preprocess_data():
+    # Veri setini yükleme ve ön işleme kodu
+    pass
+
+if __name__ == "__main__":
+    preprocess_data()
