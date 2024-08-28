45c4e95baad78344ae0ca41a73c6144ec130a90e
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 12:10:25 +0200

    Create test_data_preprocessing.py

---
+++ tests/test_data_preprocessing.py
@@ -0,0 +1,10 @@
+import unittest
+from src.data_preprocessing import preprocess_data
+
+class TestDataPreprocessing(unittest.TestCase):
+    def test_preprocess_data(self):
+        # Test veri ön işleme fonksiyonu
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
