1edb9cc3637f827369eeb7368351820cd29c2de4
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 12:03:52 +0200

    Create evaluate.py

---
+++ src/evaluate.py
@@ -0,0 +1,9 @@
+from model import create_model
+
+def evaluate_model():
+    model = create_model()
+    # Modeli yükleme ve değerlendirme kodu
+    pass
+
+if __name__ == "__main__":
+    evaluate_model()
