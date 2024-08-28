53719d0c23ba5bca2fa53635664b986ff986d31a
Olaf Yunus Laitinen-Imanzada <olaf.laitinen@gmail.com>
2024-08-28 12:10:45 +0200

    Create test_model.py

---
+++ tests/test_model.py
@@ -0,0 +1,10 @@
+import unittest
+from src.model import create_model
+
+class TestModel(unittest.TestCase):
+    def test_create_model(self):
+        # Test model oluşturma fonksiyonu
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
